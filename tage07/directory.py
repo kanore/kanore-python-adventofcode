5704c4a70a1b8fee0cd779f15dc2f84740a55a80
kanore <ordguezglez@example.com>
2022-12-19 13:20:47 -0500

    tage sieben first ideas 2

---
+++ tage07/directory.py
@@ -0,0 +1,5 @@
+class Directory:
+    def __init__(self, level = -1, name = "=", size = 0):
+        self.level = level
+        self.name = name
+        self.size = size
--- tage07/tage07.py
+++ tage07/tage07.py
@@ -1,2 +1,13 @@
 import entryTage07 as e
 
+import splitter as s
+
+import sumDir as sd
+
+directories = s.splitBy(e.entry, """
+$ ls
+""")
+
+max = 100000
+sd.sumDir(directories, max)
+
